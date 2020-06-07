486138a4c30844abd29cb0d2e321b77a8e649da0
Denis Kolosov <denis.kolosov@gmail.com>
2020-06-08 02:49:52 +0300

    Add update/delete tests

--- test/crudik/integration_test.clj
+++ test/crudik/integration_test.clj
@@ -7,32 +7,39 @@
 
 (use-fixtures :once tu/test-db-fixture) 
  
-(def ffox-driver (chrome {:headless true}))
+(def chrome-driver (chrome {:headless true}))
 
-(deftest test-create-patient-frontend
-   (go ffox-driver "http://localhost:3000/")
-   (wait-visible ffox-driver [{:tag :input}])
-   (fill ffox-driver {:tag :input :name :fullname} "Test Test")
-   (fill ffox-driver  {:tag :input :name :address} "Test Address")
-   (fill ffox-driver  {:tag :input :name :sex} "male")
-   (fill ffox-driver  {:tag :input :name :insurance} "testinsurance123")
-   (click ffox-driver {:tag :button :name :add})
-   (refresh ffox-driver)
-   (wait-visible ffox-driver [{:tag :input}])
-   (is (= (has-text? ffox-driver "Test Test") true))
-   (is (= (has-text? ffox-driver "Test Address") true))
-   (is (= (has-text? ffox-driver "male") true))
-   (is (= (has-text? ffox-driver "testinsurance123") true)))
+(deftest test-create-update-delete-patient-frontend
+   (go chrome-driver "http://localhost:3000/")
+   (wait-visible chrome-driver [{:tag :input}])
+   (fill chrome-driver {:tag :input :name :fullname} "Test Test")
+   (fill chrome-driver  {:tag :input :name :address} "Test Address")
+   (fill chrome-driver  {:tag :input :name :sex} "male")
+   (fill chrome-driver  {:tag :input :name :insurance} "testinsurance123")
+   (fill chrome-driver  {:tag :input :name :birthdate} "10-10-2020")
+   (click chrome-driver {:tag :button :name :add})
+   (refresh chrome-driver)
+   (wait-visible chrome-driver [{:tag :input}])
+   (is (= (has-text? chrome-driver "Test Test") true))
+   (is (= (has-text? chrome-driver "Test Address") true))
+   (is (= (has-text? chrome-driver "male") true))
+   (is (= (has-text? chrome-driver "testinsurance123") true))
+   (click chrome-driver {:tag :button :name :edit})
+   (fill chrome-driver {:tag :input :name :fullname} "Test Test Update")
+   (fill chrome-driver  {:tag :input :name :address} "Test Address Update")
+   (fill chrome-driver  {:tag :input :name :sex} "female")
+   (fill chrome-driver  {:tag :input :name :insurance} "testinsurance123update")
+   (fill chrome-driver  {:tag :input :name :birthdate} "11-10-2020")
+   (click chrome-driver {:tag :button :name :edit})
+   (refresh chrome-driver)
+   (wait-visible chrome-driver [{:tag :input}])
+   (wait 1)
+   (is (= (has-text? chrome-driver "Test Test Update") true))
+   (is (= (has-text? chrome-driver "Test Address Update") true))
+   (is (= (has-text? chrome-driver "female") true))
+   (is (= (has-text? chrome-driver "testinsurance123update") true))
+   (click chrome-driver {:tag :button :name :delete})
+   (refresh chrome-driver)
+   (wait-visible chrome-driver [{:tag :input}])
+   (is (= (has-text? chrome-driver "Test Test") false)))
 
-
-(deftest test-update-patient-frontend
-	(testing "Updating patients on frontend" 
-    (go ffox-driver "http://localhost:3000/")
-    (wait-visible  ffox-driver [{:tag :input}])
-    (is (= (has-text? ffox-driver "integration test") true))))
-
-(deftest test-delete-patient-frontend
-	(testing "Updating patients on frontend" 
-    (go ffox-driver "http://localhost:3000/")
-    (wait-visible  ffox-driver [{:tag :input}])
-    (is (= (has-text? ffox-driver "integration test") true))))
