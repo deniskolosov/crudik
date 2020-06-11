035c4b53f4b8047f89f5571a289c813b2fdae33d
Denis Kolosov <denis.kolosov@gmail.com>
2020-06-11 14:30:29 +0300

    Remove middleware

--- src/crudik/core.clj
+++ src/crudik/core.clj
@@ -89,9 +89,9 @@
                 :muuntaja  m/instance
                 :middleware [swagger/swagger-feature
                              muuntaja/format-middleware
-                             exception/exception-middleware
+                             ;exception/exception-middleware
                              coercion/coerce-request-middleware
-                             coercion/coerce-response-middleware
+                             ; coercion/coerce-response-middleware
                              ]}}))
 
 (def app
